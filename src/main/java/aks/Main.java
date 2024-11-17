c2c6b329ec8df2ff712a7e7bb8641f4210e5880c
04aks <abdellahssehib@gmail.com>
2024-11-17 18:39:58 +0100

    use FlatLaf theme for swing components

--- src/main/java/aks/Main.java
+++ src/main/java/aks/Main.java
@@ -1,7 +1,19 @@
 package aks;
 
+import javax.swing.UIManager;
+
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+
 public class Main {
     public static void main(String[] args) {
-        new Window();
+        try{
+            UIManager.setLookAndFeel(new FlatDarculaLaf());
+            new Window();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
